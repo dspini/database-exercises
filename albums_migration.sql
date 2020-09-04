ed8abb6f7b3128746685e03e580b3c7a88c56e69
Donald Spini <donald.spini68@gmail.com>
2020-09-04 11:38:48 -0500

    albums_migration

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,11 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist_first_name VARCHAR(50),
-    author_last_name  VARCHAR(100) NOT NULL,
-    album_name  VARCHAR(100) NOT NULL,
+    author_last_name  VARCHAR(128) NOT NULL,
+    album_name  VARCHAR(128) NOT NULL,
     release_date DATE,
     sales FLOAT,
-    genre CHAR(50),
+    genre VARCHAR(128),
     PRIMARY KEY (id)
 );
 
