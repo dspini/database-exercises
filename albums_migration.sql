d49187912d4ab7dbdfa37d135408a0b9fcdd60ad
Donald Spini <donald.spini68@gmail.com>
2020-09-04 12:13:12 -0500

    database

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,12 +2,12 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist_first_name VARCHAR(50),
-    author_last_name  VARCHAR(128) NOT NULL,
-    album_name  VARCHAR(128) NOT NULL,
-    release_date DATE,
-    sales FLOAT,
-    genre VARCHAR(128),
+    artist_first_name VARCHAR(200),
+    author_last_name  VARCHAR(200) NOT NULL,
+    album_name  VARCHAR(150) NOT NULL,
+    release_date DATE (4),
+    sales FLOAT(9),
+    genre VARCHAR(300),
     PRIMARY KEY (id)
 );
 
